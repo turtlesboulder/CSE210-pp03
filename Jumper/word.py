0e4a6e9df25ccc8433a11d618180be78efe57af5
Landon Lee <93344616+turtlesboulder@users.noreply.github.com>
2021-11-09 09:11:50 -0700

    Initial program creation
    Created the jumper ASCII art

---
+++ Jumper/word.py
@@ -0,0 +1,7 @@
+class Word:
+    def __init__(self, new_word):
+        self.word = new_word
+        self.word_display = self._make_word_display()
+
+    def _make_word_display(self):
+        pass
