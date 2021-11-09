c6498e9f46b400e0d11d84e01d71890df5fac880
Landon Lee <93344616+turtlesboulder@users.noreply.github.com>
2021-11-09 09:25:51 -0700

    added console.py
    Added the console class and added some content, also deleted sand.py

--- Jumper/sand.py
+++
@@ -1,5 +0,0 @@
-numbers = [2, 1]
-target = int(input("Enter the target\n>> "))-1
-for i in range(1,target):
-    numbers.append(numbers[i]+numbers[i-1])
-print(numbers[target])
---
+++ console.py
@@ -0,0 +1,10 @@
+class Console:
+    def __init__(self):
+        self.file_name = "{}-string-jumper.txt"
+        
+    def console_print(self, my_string:str):
+        print(my_string)
+
+    def print_image(self, num_strings):
+        # format file_name and print the file
+        pass
