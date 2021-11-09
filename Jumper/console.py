588747ecd52e225203691b68e2802ac887e41097
ronaldcoe <ronaldcoello85@gmail.com>
2021-11-09 10:58:46 -0700

    Added print strings method

--- Jumper/console.py
+++ Jumper/console.py
@@ -1,9 +1,21 @@
 class Console:
     def __init__(self):
         self.file_name = "{}-string-jumper.txt"
+
         
     def console_print(self, my_string:str):
         print(my_string)
 
-    def console_input(self):
-        return input("")
+    def get_input(self):
+        guess = input("Guess a letter: ")
+        return guess
+    
+    def print_strings(self, number):
+        self.line = open(f"{number}-string-jumper.txt")
+        for i in self.line:
+            print(i, end="")
+
+
+    
+
+    
