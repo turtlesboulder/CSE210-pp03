83569ec337aa56bba380c3554b0db78ed8133164
Evan Thomson <evanthomson1819@gmail.com>
2021-11-10 09:52:21 -0700

    Added content to director class

--- Jumper/director.py
+++ Jumper/director.py
@@ -1,3 +1,21 @@
+from console import Console
+from word import Word
+from picker import Picker
 class Director:
     def __init__(self):
+        self.console = Console()
+        self.picker = Picker()
+        self.my_word = picker.get_word()
+        self.word = Word(my_word)
+        self.num_strings = 6
+
+    def start_game(self):
+        self.console.instructions()
+        # console.parachute()
+        displayed_word = self.word.get_word_display()
+
+    def end_game(self):
+        pass
+
+    def game_loop(self):
         pass
